75273fa7b3402d61588c1cc0717fd5537c073ccd
OriLight <61224409+orilights@users.noreply.github.com>
2024-06-24 01:36:29 +0800

    chore: UI update

--- src/App.vue
+++ src/App.vue
@@ -218,14 +218,22 @@ function switchSort() {
 <template>
   <div class="flex h-screen w-screen overflow-hidden">
     <div class="flex h-full w-[300px] shrink-0 flex-col border-r">
-      <h2 class="gap-2 border-b p-2 text-xl font-bold">
+      <h2 class="flex h-[45px] items-center border-b px-2 text-xl font-bold">
         API
-        <button v-if="sort === 'name'" class="ml-2" title="按名称排序" @click="switchSort">
+        <button
+          v-if="sort === 'name'"
+          class="ml-2 transition-colors hover:text-blue-500" title="按名称排序"
+          @click="switchSort"
+        >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
           </svg>
         </button>
-        <button v-if="sort === 'update'" class="ml-2" title="按更新时间排序" @click="switchSort">
+        <button
+          v-if="sort === 'update'"
+          class="ml-2 transition-colors hover:text-blue-500" title="按更新时间排序"
+          @click="switchSort"
+        >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
           </svg>
@@ -254,7 +262,7 @@ function switchSort() {
       </div>
     </div>
     <div class="flex h-full w-[200px] shrink-0 flex-col border-r">
-      <h2 class="border-b p-2 text-xl font-bold">
+      <h2 class="flex h-[45px] items-center border-b px-2 text-xl font-bold">
         Version
       </h2>
       <div class="flex-1 overflow-y-auto">
@@ -272,28 +280,28 @@ function switchSort() {
       </div>
     </div>
     <div class="flex h-full flex-1 flex-col overflow-hidden">
-      <h2 class="border-b p-2 text-xl font-bold">
+      <h2 class="flex h-[45px] items-center border-b px-2 text-xl font-bold">
         Data
         <button
-          class="rounded-md border px-2 text-base font-medium transition-colors"
+          class="mx-2 box-border rounded-md border px-2 text-base font-medium transition-colors hover:border-gray-500"
           :class="{
-            'border-blue-500 text-blue-500': enableDiff,
+            '!border-blue-500 text-blue-500': enableDiff,
           }"
           @click="enableDiff = !enableDiff"
         >
           Diff
         </button>
-        <button class="ml-2" title="复制" @click="copyVersionData">
+        <button class="ml-2 transition-colors hover:text-blue-500" title="复制" @click="copyVersionData">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
           </svg>
         </button>
-        <button class="ml-2" title="新窗口打开" @click="openVersionData">
+        <button class="ml-2 transition-colors hover:text-blue-500" title="新窗口打开" @click="openVersionData">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
           </svg>
         </button>
-        <button class="ml-2" title="下载" @click="downloadVersionData">
+        <button class="ml-2 transition-colors hover:text-blue-500" title="下载" @click="downloadVersionData">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
           </svg>
